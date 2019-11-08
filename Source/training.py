6d2a1f3cf572401b9030067c9fe6e86ece1f0f11
Willy Rempel <willy.rempel@gmail.com>
2019-11-08 10:40:13 -0500

    change instance type, change github repo user and branch

--- Source/training.py
+++ Source/training.py
@@ -74,7 +74,7 @@ training_params = \
     },
     "ResourceConfig": {
         "InstanceCount": 1,
-        "InstanceType": "ml.p2.xlarge",
+        "InstanceType": "ml.m4.xlarge",
         "VolumeSizeInGB": 50
     },
     "TrainingJobName": job_name,
